cbcdc1cb5a250a7a2e9571f18974649b8dbc3631
null <null>
2025-08-03 15:59:02 -0400

    progress

--- main.c
+++ main.c
@@ -2,13 +2,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/syscall.h>
-#include <linux/sched.h>
-#include <linux/types.h>
 #include <errno.h>
 #include <signal.h>
 #include <string.h>
+
+#include <linux/sched.h>
+#include <linux/types.h>
+
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/mount.h>
+
+#include <dirent.h>
 
 pid_t clone() {
 	struct clone_args args;
@@ -26,7 +32,7 @@ int main() {
 	}
 	if (pid > 0) return 0;
 	
-	int fd = open("/dev/tty9", O_RDWR);
+	int fd = open("/dev/tty1", O_RDWR);
 	if (fd < 0) {
 	        perror("open");
 	        return 1;
@@ -50,10 +56,27 @@ int main() {
 	    return 1;
 	}
 
-	umount("/proc");
-	mount("proc", "/proc", "proc", 0, NULL);
+	if (mount("/var/lib/lxc/test/rootfs", "/var/lib/lxc/test/rootfs", NULL, MS_BIND | MS_REC, NULL) < 0) {
+		perror("bind /mnt/newroot");
+		return 1;
+	}
 	
-	execl("/bin/bash", "bash", (char *)NULL);
+	if (syscall(SYS_pivot_root, "/var/lib/lxc/test/rootfs", "/var/lib/lxc/test/rootfs/mnt") < 0) {
+		perror("pivot_root");
+		return 1;
+	}
+
+	if (umount2("/mnt", MNT_DETACH) < 0) {
+		perror("umount2");
+		return 1;
+	}
+
+	// umount("/proc");
+	// mount("proc", "/proc", "proc", 0, NULL);
+	
+	execl("/sbin/init", "init", (char *)NULL);
+	// execl("/bin/bash", "bash", (char *)NULL);
+	// execl("/oldroot/bin/bash", "bash", (char *)NULL);
 	// Unreachable
 }
 
